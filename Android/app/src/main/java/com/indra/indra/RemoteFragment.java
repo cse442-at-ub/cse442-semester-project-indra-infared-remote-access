208b3cd961b04c6ecd80aaa63c7ce52e62f351b8
Sam Marchant <ssmarcha@buffalo.edu>
2020-02-23 17:06:44 -0500

    Merge branch 'DEV' of C:\Users\Sam\Indra\cse442-semester-project-indra-infared-remote-access with conflicts.

---
+++ Android/app/src/main/java/com/indra/indra/RemoteFragment.java
@@ -0,0 +1,20 @@
+package com.indra.indra;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class RemoteFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_default_tv_remote, container, false);
+    }
+}
