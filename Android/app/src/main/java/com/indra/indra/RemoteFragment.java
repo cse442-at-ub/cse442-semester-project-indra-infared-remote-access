e8ece150d1ad23bc3d126cae67eebcc23c302cd1
Sam Marchant <ssmarcha@buffalo.edu>
2020-02-25 21:28:50 -0500

    #18 #13 - Added button handlers to open the settings page

--- Android/app/src/main/java/com/indra/indra/RemoteFragment.java
+++ Android/app/src/main/java/com/indra/indra/RemoteFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class RemoteFragment extends Fragment {
 
@@ -15,7 +18,19 @@ public class RemoteFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ((MainActivity) getActivity()).setMenuItemChecked(R.id.nav_remote);
+        View inflatedFragment = inflater.inflate(R.layout.fragment_default_tv_remote, container, false);
 
-        return inflater.inflate(R.layout.fragment_default_tv_remote, container, false);
+        ImageButton settingsButton = inflatedFragment.findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+                transaction.replace(R.id.fragment_container, new SettingsFragment(getString(R.string.living_room_tv))).commit();
+            }
+        });
+
+        return inflatedFragment;
     }
 }
