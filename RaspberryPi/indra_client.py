b46394cb0756030f69f62372594d426e521ffe3e
Sam Marchant <sam17marchant@gmail.com>
2020-03-24 11:00:40 -0400

    #58 #59 Integrate Pi tasks and connect to the server

--- RaspberryPi/indra_client.py
+++ RaspberryPi/indra_client.py
@@ -1,10 +1,18 @@
 import socketio
+import json
 import util.pi_lirc as pi
 
 # IP = 'cheshire.cse.buffalo.edu'
-IP = 'fathomless-brook-21291.herokuapp.com/'
+# IP = 'fathomless-brook-21291.herokuapp.com/'
+
 # PORT = '2680'
-PORT = '443'
+# PORT = '443'
+
+
+# IP = '192.168.1.15'
+# PORT = '8000'
+
+IP = 'indra-272100.appspot.com'
 
 sio = socketio.Client()
 
@@ -16,13 +24,18 @@ def connect():
 
 @sio.on('button_press')
 def my_message(data):
+    if type(data) == str:
+        data = json.loads(data)
+
     print("Message received from server with ", data)
-    res = send_ir_signal(data['remote'], data['button'])
+    print(data['remote'])
+    res = pi.send_ir_signal(data['remote'], data['button'])
     sio.emit('IRSEND Response', {'result': res})
 
 
 @sio.on('search_request')
 def handle_search_request(data):
+    data = json.loads(data)
     search_results = pi.search(data['brand'], data['model'])
 
     response = {'results': search_results, 'id': data['id']}
@@ -30,7 +43,8 @@ def handle_search_request(data):
 
 
 def main():
-    sio.connect("http://" + IP + ":" + PORT, namespaces=['/pi'])
+    # sio.connect("https://" + IP + ":" + PORT)
+    sio.connect('https://' + IP)
     sio.wait()
 
 
