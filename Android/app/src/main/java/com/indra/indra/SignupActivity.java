87c14e8e932e487c6683e709e2649bdc2e55ee07
Kartikeya Das <kkdas@buffalo.edu>
2020-04-30 14:22:23 -0400

    #19 Bugfixes(?), needs testing on other emu

--- Android/app/src/main/java/com/indra/indra/SignupActivity.java
+++ Android/app/src/main/java/com/indra/indra/SignupActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -18,6 +19,7 @@ import com.mongodb.stitch.android.core.auth.providers.userapikey.UserApiKeyAuthP
 import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoClient;
 import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoCollection;
 import com.mongodb.stitch.android.core.auth.providers.userapikey.internal.*;
+import com.mongodb.stitch.core.auth.providers.userapikey.UserApiKeyCredential;
 import com.mongodb.stitch.core.auth.providers.userapikey.models.UserApiKey;
 import com.mongodb.stitch.core.services.mongodb.remote.RemoteInsertOneResult;
 import com.mongodb.stitch.core.services.mongodb.remote.sync.ChangeEventListener;
@@ -32,11 +34,13 @@ public class SignupActivity extends AppCompatActivity {
     private Button Signup1;
     private EditText username;
     private EditText password;
+    private EditText repassword;
     private String mongoAppId = "indra-cqutq";
     private String mongoAPIKey = "6Db22acWCuY935LZz7BnERvtsdNMsOdDrDZHrRmWgjRlGTbxGRroK0dphcZAyh0T";
     private StitchAppClient stitchClient;
     private RemoteMongoClient mongoClient;
-    private RemoteMongoCollection itemsCollection;
+    private RemoteMongoCollection<Document> itemsCollection;
+    private TextView errorText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,32 +49,32 @@ public class SignupActivity extends AppCompatActivity {
 
         username = (EditText) findViewById(R.id.editText3);
         password = (EditText) findViewById(R.id.editText4);
+        repassword = (EditText) findViewById(R.id.editText5);
+        errorText = (TextView) findViewById(R.id.errorMessage2);
         Signup1 = (Button) findViewById(R.id.button2);
         authorizeDB2();
-        Signup1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                createKey(username.getText().toString(), password.getText().toString());
-            }
-        });
+            Signup1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    sign_up(username.getText().toString(), password.getText().toString());
+                }
+            });
     }
-    private void authorizeDB2(){
-        UserApiKeyAuthProviderClient apikeyclient = Stitch.getAppClient(mongoAppId).getAuth().getProviderClient(UserApiKeyAuthProviderClient.factory);
-        apikeyclient.createApiKey(mongoAPIKey).addOnCompleteListener(new OnCompleteListener<UserApiKey>() {
-            @Override
-            public void onComplete(@NonNull Task<UserApiKey> task) {
-                if (task.isSuccessful()){
-                    Log.d("Signup", "Successfully created user API: "+ task.getResult().getKey());
-                }
-                else {
-                    Log.e("Signup", "Error creating user API: "+ task.getException());
-                }
-            }
-        });
 
+    private void authorizeDB2(){
+        final StitchAppClient client = Stitch.initializeDefaultAppClient(mongoAppId);
+        client.getAuth().loginWithCredential(new UserApiKeyCredential(mongoAPIKey));
+        if(client.getAuth().isLoggedIn()) {
+            Log.d("login", "logged into db");
+        }
+        else {
+            Log.d("login", "FAILED TO LOG IN");
+        }
+        final RemoteMongoClient mongoClient = client.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas");
+        itemsCollection = mongoClient.getDatabase("indra-users").getCollection("users");
     }
-    private void createKey(String user, String pass) {
-        itemsCollection = mongoClient.getDatabase("store").getCollection("items");
+
+    private void sign_up(String user, String pass) {
         Document newDoc = new Document();
         newDoc.append("Username", user).append("Password", pass);
         final Task <RemoteInsertOneResult> insertTask = itemsCollection.insertOne(newDoc);
